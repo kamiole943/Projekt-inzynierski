2b97f2ae170063d7e8d6ce5227fe75679eda8083
kamiole943 <berthold1995@gmail.com>
2017-10-26 17:46:26 +0200

    speech-to-text

--- Project/app/src/main/java/com/example/isengrim/project/numbers.java
+++ Project/app/src/main/java/com/example/isengrim/project/numbers.java
@@ -1,6 +1,9 @@
 package com.example.isengrim.project;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,10 +12,14 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Random;
 
 
@@ -21,7 +28,7 @@ public class numbers extends AppCompatActivity {
 
     Button ok, b1, b2, b3, b4, b5, b6, b7, b8, b9, bC;
     TextView generated_number, inform;
-
+    ImageButton bvoice;
     int steering = 0;
     int length = 3;
     String number = intarray2string(random_number(length));
@@ -34,7 +41,10 @@ public class numbers extends AppCompatActivity {
         final Button ok = (Button) findViewById(R.id.ok);
         final TextView generated_number = (TextView) findViewById(R.id.generated_number);
         final TextView inform = (TextView) findViewById(R.id.inform);
+        final ImageButton bvoice = (ImageButton) findViewById(R.id.bvoice);
+
         generated_number.setText(number);
+
         inform.setText("Rozgrywka została rozpoczeta. Proszę zapamiętać podaną liczbę.");
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +71,16 @@ public class numbers extends AppCompatActivity {
             }
 
         });
+        bvoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(steering==1) {
+                    promptSpeechInput();
+                }
+            }
+
+        });
+
     }
     public void ButtonClicked(View view) {
         TextView generated_number = (TextView) findViewById(R.id.generated_number);
@@ -106,6 +126,42 @@ public class numbers extends AppCompatActivity {
         }
         }
 
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Powiedz liczbę");
+        try {
+            startActivityForResult(intent, 100);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), "Wykrywanie mowy niewspomagane", Toast.LENGTH_LONG).show();
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TextView generated_number = (TextView) findViewById(R.id.generated_number);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 100: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    if( isValidInteger(result.get(0))) {
+                        generated_number.setText(result.get(0));
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(),"Liczba została nieprawnie wypowiedziana",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            }
+
+        }
+    }
+
+
     public int[] random_number(int length)
     {
         int[] number = new int[length];
@@ -139,6 +195,17 @@ public class numbers extends AppCompatActivity {
         else
         {
             inform.setText("Liczba nie została poprawnie zapamiętana, oto kolejna:");
+            return false;
+        }
+    }
+    public static Boolean isValidInteger(String value) {
+        try {
+            Integer val = Integer.valueOf(value);
+            if (val != null)
+                return true;
+            else
+                return false;
+        } catch (NumberFormatException e) {
             return false;
         }
     }
